d199192245bf4d0255973046e943c905b15bd8e1
stibu81 <lanz.stefan@gmx.ch>
2019-07-29 17:40:24 +0200

    use quiz in shiny app

--- R/shiny.R
+++ R/shiny.R
@@ -29,19 +29,20 @@ ui <- fluidPage(
 
         # main panel
         mainPanel(
-            textOutput("current_box"),
-            textOutput("current_group")
-            #
-            #  # written mode
-            #  conditionalPanel(
-            #      condition = "input.mode == 'written'",
-            #      tableOutput("wordlist")
-            # ),
-            # # oral mode
-            # conditionalPanel(
-            #      condition = "input.mode == 'oral'",
-            #      tableOutput("wordlist")
-            # )
+             "Anzahl richtige Antworten: ",
+            div(style = "display:inline-block", textOutput("n_correct")),
+            br(), "Anzahl falsche Antworten: ",
+            div(style = "display:inline-block", textOutput("n_wrong")),
+            br(), br(),
+            "Fach: ",
+            div(style = "display:inline-block", textOutput("current_box")),
+            br(), "Gruppe: ",
+            div(style = "display:inline-block", textOutput("current_group")),
+            br(), br(),
+            strong("Aufgabe"),
+            textOutput("question"),
+            br(),
+            uiOutput("exerciseUI")
         )
     )
 )
@@ -49,58 +50,124 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
 
-    # load a file
-    wl <- reactive({
-        file <- input$wordlist_file
-        if (is.null(file)) {
-            return(NULL)
-        } else {
-            return(read_wordlist(file$datapath))
-        }
-    })
+    # create an object to track the state of the application
+    state <- reactiveValues(running = FALSE,
+                            wl = NULL,
+                            quiz = NULL,
+                            question = 0,
+                            i_exercise = 0,
+                            show_answer = FALSE,
+                            n_correct = 0,
+                            n_wrong = 0)
 
-    # update selection box for direction
+    # load a file
     observe({
-        if (!is.null(wl())) {
-            langs <- get_languages(wl())
+        file <- input$wordlist_file
+        if (!is.null(file)) {
+            state$wl <- read_wordlist(file$datapath)
+            langs <- get_languages(state$wl)
             choices <- magrittr::set_names(paste0("direction", 1:2),
                                            paste0(langs, " > ", rev(langs)))
             updateSelectInput(session, "direction", choices = choices)
         }
     })
 
-    current <- reactive({
-        wl()[1, ]
+    output$current_box <- renderText({
+        if (state$running) state$question$box
     })
 
-    output$current_box <- reactive({
-        if (input$run > 0) {
-            if (input$direction == "direction1") {
-                paste("Fach:", current()$box1)
-            } else {
-                paste("Fach:", current()$box2)
-            }
-        } else {
-            "Fach:"
-        }
+    output$question <- renderText({
+        if (state$running) state$question$question
     })
-    output$current_group <- reactive({
-        if (input$run > 0) {
-            paste("Gruppe:", current()$group)
+
+    output$current_group <- renderText({
+        if (state$running) state$question$group
+    })
+
+    output$n_correct <- renderText({
+        if (state$running) state$n_correct
+    })
+
+    output$n_wrong <- renderText({
+        if (state$running) state$n_wrong
+    })
+
+
+    # start the exercise
+    observeEvent(input$run,
+        if (!state$running) {
+            state$running <- TRUE
+            cat("running exercise with",
+                input$direction, "and mode", input$mode)
+            direction <- gsub("direction", "", input$direction) %>%
+                            as.numeric()
+            state$quiz <- prepare_quiz(state$wl, direction)
+            state$i_exercise <- state$i_exercise + 1
+        }
+    )
+
+    # create the UI when the exercise is started
+    output$exerciseUI <- renderUI({
+        if (!state$running) return(NULL)
+        if (input$mode == "written") {
+            tagList(
+                textInput("solution_in", "Übersetzung"),
+                actionButton("check_written", "Prüfen"),
+                br(), br(),
+                strong("Lösung"),
+                textOutput("solution"),
+                br(),
+                actionButton("gonext", "Weiter")
+            )
         } else {
-            "Gruppe:"
+            tagList(
+                actionButton("check", "Prüfen"),
+                br(), br(),
+                strong("Lösung"),
+                textOutput("solution"),
+                br(),
+                actionButton("correct", "Richtig"),
+                actionButton("wrong", "Falsch")
+            )
         }
     })
 
-    # create diagnostic output, if the run
-    # button is clicked for the first time
-    observe({
-        if (input$run == 1) {
-            cat("Starting exercise with direction",
-                input$direction, "and mode",
-                input$mode, file = stderr())
+    # initialise a new exercise
+    observeEvent(state$i_exercise, {
+        if (state$running) {
+            state$question <- draw_question(state$quiz, state$wl)
         }
     })
+
+    # button check: show the solution
+    observeEvent(input$check,
+        if (state$running) state$show_answer <- TRUE)
+    output$solution <- renderText({
+        if (state$show_answer) {
+            state$question$answer
+        } else {
+            " "
+        }
+    })
+
+    # buttons next, correct, wrong
+    observeEvent(input$gonext, {
+        state$show_answer <- FALSE
+        state$i_exercise <- state$i_exercise + 1
+    })
+    observeEvent(input$correct, {
+        state$show_answer <- FALSE
+        state$n_correct <- state$n_correct + 1
+        state$i_exercise <- state$i_exercise + 1
+    })
+    observeEvent(input$wrong, {
+        state$show_answer <- FALSE
+        state$n_wrong <- state$n_wrong + 1
+        state$i_exercise <- state$i_exercise + 1
+    })
+
+
+
 }
 
 #' Run the application
